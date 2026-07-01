2c537cf9688407570c0ea40ccd8ef2772da55a9d
Garrett Gao <1053836@lwsd.org>
2026-07-01 11:34:53 -0400

    Added alarms and explosion sfx and the code for some of them

--- Reactor Game/music_module.py
+++ Reactor Game/music_module.py
@@ -27,6 +27,9 @@ class Music():
                 pygame.mixer.music.load("sound/meltdown1.mp3")
                 self.active_music = "meltdown"
                 pygame.mixer.music.play(-1, start = 292.0)
+                self.alarm3 = pygame.mixer.Sound("sfx/alarm3.mp3")
+                self.alarm3.set_volume(0.7)
+                self.alarm3.play(-1)
             if new_music == "operational":
                 pygame.mixer.music.fadeout(1000)
                 pygame.mixer.music.load("sound/operational1.mp3")
@@ -47,6 +50,8 @@ class Music():
                 pygame.mixer.music.load("sound/detonation1.mp3")
                 self.active_music = "detonation"
                 pygame.mixer.music.play(-1, start = 22.0)
+                self.alarm1 = pygame.mixer.Sound("sfx/alarm1.mp3")
+                self.alarm1.play(-1)
             if new_music == "shutdown":
                 pygame.mixer.music.load("sound/shutdown1.mp3")
                 self.active_music = "shutdown"
